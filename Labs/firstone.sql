2d913367e5308afb3a3f0761596c204274ca798a
KeopsKps <christhofer_gutierrez@outlook.com>
2018-05-22 16:34:15 -0600

    22/5/18 Second

--- Labs/firstone.sql
+++ Labs/firstone.sql
@@ -88,3 +88,77 @@ GROUP BY 'Nombre Trabajador');
 UPDATE Proyecto
 SET NoRecibo = 'Nuevo numero de recibo'
 WHERE CedulaTrabajador = 'Cedula de un trabajador';
+
+-- Eliminar registro especifico
+DELETE FROM Proyecto
+WHERE IdProyecto = 12 AND CedulaTrabajador = 'Una cedula de trabajador';
+
+
+-- Tabla: Cliente
+-- Campos: CedulaUsuario(VARCHAR(16)), Nombre(VARCHAR(25)), Apellido(VARCHAR(25))
+-- Direccion(VARCHAR(200))
+
+-- Agregar un cliente nuevo
+INSERT INTO Cliente VALUES(
+        'Cedula cliente',
+        'Nombre cliente',
+        'Apellido cliente',
+        'Dirección cliente'
+);
+
+-- Mostrar todos los clientes
+SELECT CedulaUsuario as 'Cedula de Usuario',
+       Nombre as 'Nombre usuario',
+       Apellido,
+       Direccion as 'Dirección'
+FROM Cliente
+ORDER BY 'Nombre usuario' DESC;
+
+-- Actualizar la direccion 
+UPDATE Cliente
+SET Direccion = 'Nueva dirección'
+WHERE CedulaUsuario = 'Cedula de cliente';
+
+-- Mostrar los clientes cuyos nombres empiezen con la letra A
+SELECT Nombre as 'Nombre cliente'
+FROM Cliente
+WHERE 'Nombre cliente' LIKE 'A%'
+ORDER BY 'Nombre cliente';
+
+-- Mostrar los clientes que hayan nacido en 1996
+SELECT CedulaUsuario as 'Cedula Usuario',
+       Nombre,
+       Apellido
+FROM Cliente
+WHERE CedulaUsuario LIKE '___-____96-_____'
+ORDER BY Nombre DESC;
+
+
+-- Tabla: TelefonoCliente
+-- Campos: Numero1(VARCHAR(30)), NumeroReferencia(VARCHAR(30)), Cedula(VARCHAR(16))
+
+-- Añadir telefeonos a clientes
+INSERT INTO TelefonoCliente VALUES(
+        'Numero 1',
+        'Numero de referencia',
+        'Cedula de cliente'
+);
+
+-- Seleccionar todos los clientes con sus numeros de telefonos
+SELECT Nombre,
+       Apellido,
+       Numero1 as 'Numero de Cliente'
+FROM Cliente
+INNER JOIN TelefonoCliente ON Cliente.CedulaUsuario = TelefonoCliente.Cedula
+ORDER BY Nombre DESC;
+
+-- Muestra todos los numeros de telefonos de clientes
+SELECT Numero1 as 'Numero',
+       NumeroReferencia as 'Numero de Referencia',
+       Cedula
+FROM TelefonoCliente
+ORDER BY Cedula;
+
+-- Elimina todos los numeros de un cliente
+DELETE FROM TelefonoCliente
+WHERE Cedula = 'numero de cedula de cliente';
