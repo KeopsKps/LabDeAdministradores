dfc54718367ab95ad54e160193c52e85c68ade14
KeopsKps <christhofer_gutierrez@outlook.com>
2018-05-22 01:05:50 -0600

    Avance 1

--- Labs/firstone.sql
+++ Labs/firstone.sql
@@ -6,4 +6,64 @@
 -- Campos: NoAlcaldia(VARCHAR(25)), NoMTI(VARCHAR(25)), CedulaTrabajador(VARCHAR(16))
 -- Nombre(VARCHAR(25)), Apellido(VARCHAR(25))
 
--- 
+-- Insercion de trabajadores
+INSERT INTO Trabajador VALUES (
+    'Agregar Numero de alcaldia',
+    'Agregar Numero de MTI',
+    'Agregar CedulaTrabajor',
+    'Nombre de trabajador',
+    'Apellido de trabajador'
+);
+
+-- Seleccion de trabajadores
+SELECT NoAlcaldia as 'Numero de alcaldia',
+       NoMTI as 'Numero de MTI',
+       CedulaTrabajador as 'Cedula',
+       Nombre as 'Nombre de trabajador',
+       Apellido as 'Apellidos'
+FROM Trabajador
+ORDER BY 'Nombre de trabajador' DESC;
+
+-- Eliminacion de trabajador con cedula 'xxx-xxxxxx-xxx'
+DELETE FROM Trabajador
+WHERE CedulaTrabajador = 'xxx-xxxxxx-xxx';
+
+-- Actualizacion de nombre de trabajador
+UPDATE Trabajar    
+SET Nombre = 'Nombre nuevo'
+WHERE CedulaTrabajador = 'xxx-xxxxxx-xxx';
+
+-- Selecciona los trabajadores cuyos nombres empiezan con 'N'
+SELECT CedulaTrabajdor as 'Cedula de trabajador',
+       Nombre as 'Nombre',
+       Apellido as 'Apellido'
+FROM Trabajador
+WHERE 'Nombre' LIKE 'N%';
+
+
+
+-- Tabla: Proyecto
+-- Campos: idProyecto(INT), NoRecibo(VARCHAR(30)), CedulaTrabajador(VARCHAR(16))
+
+-- Insercion de proyectos
+INSERT INTO Proyecto VALUES (
+    1,
+    'Numero de recibo',
+    'Cedula de trabajador'
+);
+
+-- Seleccionar todos los proyectos
+SELECT idProyecto as 'Identificador proyecto',
+       NoRecibo as 'Numero Recibo',
+       CedulaTrabajador as 'Cedula de trabajador'
+FROM Proyecto
+ORDER BY 'Identificador proyecto' DESC
+
+-- Selecciona los trabajadores que tienen proyecto mostrar:
+-- nombre, apellido y id del proyecto
+SELECT Nombre as 'Nombre trabajador',
+       Apellido,
+       idProyecto as 'Identificador de proyecto'
+FROM Trabajador
+INNER JOIN Proyecto ON Trabajador.CedulaTrabajador = Proyecto.CedulaTrabajador
+ORDER BY 'Identificador de proyecto'
