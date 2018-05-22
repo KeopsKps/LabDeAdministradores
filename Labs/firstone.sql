09eeb95a8715cb40921182ed4b89fa00b538c49d
KeopsKps <christhofer_gutierrez@outlook.com>
2018-05-21 22:37:26 -0600

    added first lines

---
+++ Labs/firstone.sql
@@ -0,0 +1,9 @@
+-- Primer laboratorio:
+-- 10 consultas por cada tabla incluyendo: SELECT, INSERT, DELETE, UPDATE
+
+
+-- Tabla: Trabajador
+-- Campos: NoAlcaldia(VARCHAR(25)), NoMTI(VARCHAR(25)), CedulaTrabajador(VARCHAR(16))
+-- Nombre(VARCHAR(25)), Apellido(VARCHAR(25))
+
+-- 
